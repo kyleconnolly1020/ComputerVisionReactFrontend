b7e66c45af56478ea5e0e56aefeabdfc82b3f6ef
Kyle Connolly <kyleconnolly1020@gmail.com>
2022-04-20 18:00:32 -0700

    finished project with react

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react"
+import React, { useEffect, useRef, useState } from "react"
 import NavBar from "./components/AppBar"
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
@@ -8,14 +8,51 @@ import Grid from '@mui/material/Grid'
 import './App.css';
 
 function App() {
+  const videoRef = useRef(null)
+  const photoRef = useRef(null)
   const [colors, setColors] = useState({
     red: false,
     green: false,
     blue: false
   });
 
-  const getColors = (e) => {
-    fetch('http://localhost:5000/capture').then((res) => {
+  const getVideo = () => {
+    navigator.mediaDevices
+      .getUserMedia({
+        video: { width: 1920, height: 1080 }
+      })
+      .then(stream => {
+        let video = videoRef.current;
+        video.srcObject = stream;
+        video.play();
+      })
+      .catch(err => {
+        console.error(err);
+      })
+  }
+
+  const getColors = async () => {
+    const width = 414;
+    const height = width / (16 / 9);
+
+    let video = videoRef.current;
+    let photo = photoRef.current;
+
+    photo.width = width;
+    photo.height = height;
+
+    let ctx = photo.getContext('2d');
+    await ctx.drawImage(video, 0, 0, width, height)
+
+    let base64 = photo.toDataURL()
+
+    await fetch('https://api-computervision.herokuapp.com/capture', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json;charset=utf-8'
+      },
+      body: JSON.stringify({ photo: base64 })
+    }).then(res => {
       res.json().then((data) => {
         setColors({
           red: data.Red,
@@ -23,18 +60,44 @@ function App() {
           blue: data.Blue
         });
       })
-    })
+    });
+
+    // let result = response.json()
+    // console.log(result)
+
+    // setColors({
+    //   red: result.Red,
+    //   green: result.Green,
+    //   blue: result.Blue
+    // })
   }
+
+  // const getColors = (e) => {
+  //   fetch('http://localhost:5000/capture').then((res) => {
+  //     res.json().then((data) => {
+  //       setColors({
+  //         red: data.Red,
+  //         green: data.Green,
+  //         blue: data.Blue
+  //       });
+  //     })
+  //   })
+  // }
+
+  useEffect(() => {
+    getVideo();
+  }, [videoRef])
 
   return (
     <div className="App">
       <NavBar />
       <header className="App-header">
         <h3><Typography variant="h4" className="app_blurb">Identify the RGB colors in your stream</Typography></h3>
-        <img alt="streamwindow" className="stream_window" src={"https://api-computervision.herokuapp.com/video_feed"} />
+        <video alt="streamwindow" className="stream_window" ref={videoRef}></video>
+        <canvas className="imageCanvas" ref={photoRef}></canvas>
         <br />
         <Button className="identify_button" onClick={getColors} variant="contained" startIcon={<ColorLensIcon />}>Identify Colors</Button>
-        
+
         <br />
         <Grid className="colorGrid" container spacing={1}>
           {colors.red && <Grid item><Chip className="color_chip" label="RED" color="error" /></Grid>}
